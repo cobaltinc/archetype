4c0d523be746dc46a58194eb7bfcc626f3a52b78
kciter <kciter@naver.com>
2022-08-12 08:34:06 +0900

    숙제가 있는 것을 깜빡

--- quantity/src/main/kotlin/run/cobalt/archetype/quantity/Dimension.kt
+++ quantity/src/main/kotlin/run/cobalt/archetype/quantity/Dimension.kt
@@ -1,4 +1,13 @@
 package run.cobalt.archetype.quantity
 
-class Dimension {
+open class Dimension private constructor(val unit: Char) {
+  companion object {
+    val LENGTH = Dimension('L')
+    val MASS = Dimension('M')
+    val TIME = Dimension('T')
+    val ELECTRIC_CURRENT = Dimension('I')
+    val TEMPERATURE = Dimension('Θ')
+    val AMOUNT_OF_SUBSTANCE = Dimension('N')
+    val LUMINOUS_INTENSITY = Dimension('J')
+  }
 }
---
+++ quantity/src/main/kotlin/run/cobalt/archetype/quantity/Term.kt
@@ -0,0 +1,4 @@
+package run.cobalt.archetype.quantity
+
+class Term(val dimension: Dimension, val power: Int) {
+}
