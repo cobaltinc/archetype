2bba8253b898d5c68f077198eadc85f6be62b895
Y <y@cobalt.run>
2022-08-12 09:57:52 +0900

    Implement Element class and its tests.

--- quantity/src/main/kotlin/run/cobalt/archetype/quantity/Dimension.kt
+++ quantity/src/main/kotlin/run/cobalt/archetype/quantity/Dimension.kt
@@ -1,4 +1,39 @@
 package run.cobalt.archetype.quantity
 
+@JvmInline
+value class Symbol(private val symbol: Char)
+
 class Dimension {
+  class Element private constructor(val symbol: Symbol, val definition: String? = null) {
+
+    class AlreadyDefined : IllegalArgumentException()
+    class Undefined : IllegalArgumentException()
+
+    companion object {
+      private val lock: Any = Any()
+      private val pool: MutableMap<Symbol, Element> = mutableMapOf()
+
+      val LENGTH: Element = create(symbol = Symbol('L'), definition = "length")
+      val MASS = create(symbol = Symbol('M'), definition = "mass")
+      val TIME = create(symbol = Symbol('T'), definition = "time")
+      val ELECTRIC_CURRENT = create(symbol = Symbol('I'), definition = "electric current")
+      val TEMPERATURE = create(symbol = Symbol('Θ'), definition = "thermodynamic temperature")
+      val AMOUNT_OF_SUBSTANCE = create(symbol = Symbol('N'), definition = "amount of substance")
+      val LUMINOUS_INTENSITY = create(symbol = Symbol('J'), definition = "luminous intensity")
+      val DIMENSIONLESS = create(symbol = Symbol('1'), definition = "dimensionless")
+
+      fun create(symbol: Symbol, definition: String? = null): Element {
+        synchronized(lock) {
+          if (pool.containsKey(symbol)) throw AlreadyDefined()
+          val element = Element(symbol, definition)
+          pool[symbol] = element
+          return element
+        }
+      }
+
+      fun of(symbol: Symbol): Element {
+        return pool[symbol] ?: throw Undefined()
+      }
+    }
+  }
 }
--- quantity/src/test/kotlin/run/cobalt/archetype/quantity/DimensionTest.kt
+++ quantity/src/test/kotlin/run/cobalt/archetype/quantity/DimensionTest.kt
@@ -1,6 +1,51 @@
 package run.cobalt.archetype.quantity
 
-import io.kotest.core.spec.style.WordSpec
+import io.kotest.core.spec.style.FreeSpec
+import org.junit.jupiter.api.assertThrows
 
-class DimensionTest: WordSpec({
+class DimensionTest : FreeSpec({
+  "Dimension Element" - {
+
+    "Clients can create their own element." - {
+      val e1 = Dimension.Element.create(symbol = Symbol('X'), definition = "Custom")
+
+      "If client try creating an element with a symbol which is already defined, throw an AlreadyDefined exception." {
+        assertThrows<Dimension.Element.AlreadyDefined> {
+          Dimension.Element.create(
+            symbol = Symbol('X'),
+            definition = "Custom"
+          )
+        }
+      }
+
+      "Clients can get an element with a symbol." {
+        val e2 = Dimension.Element.of(Symbol('X'))
+        assert(e2.symbol == Symbol('X'))
+      }
+
+      "if the symbols of elements are same, this elements must be identical object." {
+        val e2 = Dimension.Element.of(symbol = Symbol('X'))
+        assert(e1 === e2)
+      }
+    }
+
+    "A client can access 8 predefined primitive elements, and the primitive elements are singletons." {
+      val l = Dimension.Element.of(Symbol('L'))
+      assert(l === Dimension.Element.LENGTH)
+      val m = Dimension.Element.of(Symbol('M'))
+      assert(m === Dimension.Element.MASS)
+      val t = Dimension.Element.of(Symbol('T'))
+      assert(t === Dimension.Element.TIME)
+      val i = Dimension.Element.of(Symbol('I'))
+      assert(i === Dimension.Element.ELECTRIC_CURRENT)
+      val Θ = Dimension.Element.of(Symbol('Θ'))
+      assert(Θ === Dimension.Element.TEMPERATURE)
+      val n = Dimension.Element.of(Symbol('N'))
+      assert(n === Dimension.Element.AMOUNT_OF_SUBSTANCE)
+      val j = Dimension.Element.of(Symbol('J'))
+      assert(j === Dimension.Element.LUMINOUS_INTENSITY)
+      val z = Dimension.Element.of(Symbol('1'))
+      assert(z === Dimension.Element.DIMENSIONLESS)
+    }
+  }
 })
